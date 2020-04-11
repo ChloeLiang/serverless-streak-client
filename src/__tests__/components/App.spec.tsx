2adc195133232e40c633839a682f239b8dc9cae6
ChloeLiang <nliangxin@gmail.com>
2020-04-11 10:58:06 +0800

    add unit tests for sign up page

--- src/__tests__/components/App.spec.tsx
+++ src/__tests__/components/App.spec.tsx
@@ -14,6 +14,7 @@ import ERROR from '../../constants/error';
 jest.mock('aws-amplify');
 
 afterEach(cleanup);
+afterEach(jest.clearAllMocks);
 
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
@@ -175,4 +176,169 @@ it('should redirect user to login page after logout', async () => {
   await waitForElementToBeRemoved(() => screen.queryByText(/Logout/i));
   expect(screen.queryByText(/Username/i)).toBeTruthy();
   expect(screen.queryByText(/Password/i)).toBeTruthy();
+});
+
+it('should render sign up form', async () => {
+  awsAmplify.Auth.currentSession.mockImplementationOnce(() => {
+    return Promise.reject('No current user');
+  });
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  await waitForElementToBeRemoved(() => screen.queryByText(/Loading/i));
+  fireEvent.click(screen.getByText(/Sign Up/i));
+  expect(screen.queryByText(/Username/i)).toBeTruthy();
+  expect(screen.queryAllByText(/Password/i)).toHaveLength(2);
+});
+
+it('should sign up, confirm code and redirect to home page', async () => {
+  awsAmplify.Auth.currentSession.mockImplementationOnce(() => {
+    return Promise.reject('No current user');
+  });
+  awsAmplify.Auth.confirmSignUp.mockImplementationOnce(() => {
+    return Promise.resolve('success');
+  });
+  awsAmplify.Auth.signIn.mockImplementationOnce(() => {
+    return Promise.resolve('success');
+  });
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  await waitForElementToBeRemoved(() => screen.queryByText(/Loading/i));
+  fireEvent.click(screen.getByText(/Sign Up/i));
+  const usernameInput = screen.getByLabelText(/Username/i);
+  const passwordInput = screen.getAllByLabelText(/Password/i)[0];
+  const confirmPasswordInput = screen.getByLabelText(/Confirm Password/i);
+  const username = 'admin@example.com';
+  const password = 'Passw0rd!';
+  const confirmationCode = '111111';
+
+  // Sign up and show confirmation code page
+  fireEvent.change(usernameInput, { target: { value: username } });
+  fireEvent.change(passwordInput, { target: { value: password } });
+  fireEvent.change(confirmPasswordInput, { target: { value: password } });
+  fireEvent.click(screen.getByTestId('signUp-submit'));
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/Confirm Password/i)
+  );
+  expect(awsAmplify.Auth.signUp).toHaveBeenCalledTimes(1);
+  expect(awsAmplify.Auth.signUp).toHaveBeenCalledWith({ username, password });
+  expect(screen.queryByText(/Confirmation Code/i)).toBeTruthy();
+
+  // Confirm code and redirect to home page
+  const codeInput = screen.getByLabelText(/Confirmation Code/i);
+  fireEvent.change(codeInput, { target: { value: confirmationCode } });
+  fireEvent.click(screen.getByTestId('signUp-verify'));
+  await screen.findByText(/Logout/i);
+  expect(awsAmplify.Auth.confirmSignUp).toHaveBeenCalledTimes(1);
+  expect(awsAmplify.Auth.confirmSignUp).toHaveBeenCalledWith(
+    username,
+    confirmationCode
+  );
+});
+
+it('should stay on sign up page if sign up failed', async () => {
+  const signUpError = 'Error signing up';
+  awsAmplify.Auth.currentSession.mockImplementationOnce(() => {
+    return Promise.reject('No current user');
+  });
+  awsAmplify.Auth.signUp.mockImplementationOnce(() => {
+    return Promise.reject({ message: signUpError });
+  });
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  await waitForElementToBeRemoved(() => screen.queryByText(/Loading/i));
+  fireEvent.click(screen.getByText(/Sign Up/i));
+  const usernameInput = screen.getByLabelText(/Username/i);
+  const passwordInput = screen.getAllByLabelText(/Password/i)[0];
+  const confirmPasswordInput = screen.getByLabelText(/Confirm Password/i);
+  const username = 'admin@example.com';
+  const password = 'Passw0rd!';
+  fireEvent.change(usernameInput, { target: { value: username } });
+  fireEvent.change(passwordInput, { target: { value: password } });
+  fireEvent.change(confirmPasswordInput, { target: { value: password } });
+  fireEvent.click(screen.getByTestId('signUp-submit'));
+  await screen.findByText(signUpError);
+  expect(screen.queryByText(/Logout/i)).toBeFalsy();
+  expect(screen.queryByText(/Sign up/i)).toBeTruthy();
+  expect(screen.queryByText(/Login/i)).toBeTruthy();
+});
+
+it('should show verify page if user exists but is not verified', async () => {
+  awsAmplify.Auth.currentSession.mockImplementationOnce(() => {
+    return Promise.reject('No current user');
+  });
+  awsAmplify.Auth.signUp.mockImplementationOnce(() => {
+    return Promise.reject({ code: 'UsernameExistsException' });
+  });
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  await waitForElementToBeRemoved(() => screen.queryByText(/Loading/i));
+  fireEvent.click(screen.getByText(/Sign Up/i));
+  const usernameInput = screen.getByLabelText(/Username/i);
+  const passwordInput = screen.getAllByLabelText(/Password/i)[0];
+  const confirmPasswordInput = screen.getByLabelText(/Confirm Password/i);
+  const username = 'admin@example.com';
+  const password = 'Passw0rd!';
+
+  fireEvent.change(usernameInput, { target: { value: username } });
+  fireEvent.change(passwordInput, { target: { value: password } });
+  fireEvent.change(confirmPasswordInput, { target: { value: password } });
+  fireEvent.click(screen.getByTestId('signUp-submit'));
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/Confirm Password/i)
+  );
+  expect(awsAmplify.Auth.signUp).toHaveBeenCalledTimes(1);
+  expect(awsAmplify.Auth.signUp).toHaveBeenCalledWith({ username, password });
+  expect(awsAmplify.Auth.resendSignUp).toHaveBeenCalledTimes(1);
+  expect(awsAmplify.Auth.resendSignUp).toHaveBeenCalledWith(username);
+  expect(screen.queryByText(/Confirmation Code/i)).toBeTruthy();
+});
+
+it('should show verification error', async () => {
+  const verificationError = 'error';
+  awsAmplify.Auth.currentSession.mockImplementationOnce(() => {
+    return Promise.reject('No current user');
+  });
+  awsAmplify.Auth.confirmSignUp.mockImplementationOnce(() => {
+    return Promise.reject({ message: verificationError });
+  });
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  await waitForElementToBeRemoved(() => screen.queryByText(/Loading/i));
+  fireEvent.click(screen.getByText(/Sign Up/i));
+  const usernameInput = screen.getByLabelText(/Username/i);
+  const passwordInput = screen.getAllByLabelText(/Password/i)[0];
+  const confirmPasswordInput = screen.getByLabelText(/Confirm Password/i);
+  const username = 'admin@example.com';
+  const password = 'Passw0rd!';
+  const confirmationCode = '111111';
+
+  // Sign up and show confirmation code page
+  fireEvent.change(usernameInput, { target: { value: username } });
+  fireEvent.change(passwordInput, { target: { value: password } });
+  fireEvent.change(confirmPasswordInput, { target: { value: password } });
+  fireEvent.click(screen.getByTestId('signUp-submit'));
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/Confirm Password/i)
+  );
+
+  const codeInput = screen.getByLabelText(/Confirmation Code/i);
+  fireEvent.change(codeInput, { target: { value: confirmationCode } });
+  fireEvent.click(screen.getByTestId('signUp-verify'));
+  const error = await screen.findByText(verificationError);
+  expect(error).toBeTruthy();
 });
---
+++ src/__tests__/components/SignUp.spec.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import {
+  render,
+  cleanup,
+  fireEvent,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import SignUp from '../../components/SignUp';
+import ERROR from '../../constants/error';
+import routerProps from '../../__mocks__/router-props';
+
+jest.mock('aws-amplify');
+
+afterEach(cleanup);
+
+it('should render basic component', () => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // deprecated
+      removeListener: jest.fn(), // deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+  const { container } = render(<SignUp {...routerProps} />);
+  expect(container.firstChild).toMatchSnapshot();
+});
+
+it('should validate username', async () => {
+  render(<SignUp {...routerProps} />);
+  const usernameInput = screen.getByLabelText(/Username/i);
+  const button = screen.getByTestId('signUp-submit');
+
+  fireEvent.click(button);
+  const requiredError = await screen.findByText(ERROR.REQUIRED_USERNAME);
+  expect(requiredError).toBeTruthy();
+
+  fireEvent.change(usernameInput, { target: { value: 'test' } });
+  const invalidError = await screen.findByText(ERROR.INVALID_USERNAME);
+  expect(invalidError).toBeTruthy();
+
+  fireEvent.change(usernameInput, { target: { value: 'example@gmail.com' } });
+  const result = await waitForElementToBeRemoved(() =>
+    screen.queryByText(ERROR.INVALID_USERNAME)
+  );
+  expect(result).toBeTruthy();
+});
+
+it('should validate password', async () => {
+  render(<SignUp {...routerProps} />);
+  const button = screen.getByTestId('signUp-submit');
+
+  fireEvent.click(button);
+  const requiredError = await screen.findAllByText(ERROR.REQUIRED_PASSWORD);
+  expect(requiredError).toHaveLength(2);
+});
+
+it('should validate confirm password', async () => {
+  render(<SignUp {...routerProps} />);
+  const button = screen.getByTestId('signUp-submit');
+  const usernameInput = screen.getByLabelText(/Username/i);
+  const passwordInput = screen.getAllByLabelText(/Password/i)[0];
+  const confirmPasswordInput = screen.getByLabelText(/Confirm Password/i);
+  const username = 'admin@example.com';
+  const password = 'Passw0rd!';
+  const wrongPassword = 'Password';
+  fireEvent.change(usernameInput, { target: { value: username } });
+  fireEvent.change(passwordInput, { target: { value: password } });
+  fireEvent.change(confirmPasswordInput, { target: { value: wrongPassword } });
+  fireEvent.click(button);
+  await screen.findByText(ERROR.CONFIRM_PASSWORD);
+});
