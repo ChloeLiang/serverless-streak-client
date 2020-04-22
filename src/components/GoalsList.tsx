47fabd736d53fa39259d47161159fb45fe0560d0
ChloeLiang <nliangxin@gmail.com>
2020-04-22 20:58:14 +0800

    make goals list scrollable

--- src/components/GoalsList.tsx
+++ src/components/GoalsList.tsx
@@ -16,52 +16,54 @@ const GoalsList: FunctionComponent<Props> = (props) => {
   return (
     <div className="GoalsList">
       <h4>{props.title}</h4>
-      {props.goals.map((goal: GoalResponse) => {
-        const {
-          title,
-          startDate,
-          endDate,
-          amount,
-          checklist,
-          progress,
-        } = goal.content;
-        const isStarted = shouldShowProgress(
-          startDate,
-          endDate,
-          amount,
-          checklist
-        );
-        const totalAmount = amount || (checklist && checklist.length) || 0;
-        const targetProgress = getTargetProgress(
-          startDate,
-          endDate,
-          totalAmount
-        );
-        const { targetPercent, successPercent } = getProgressPercent(
-          progress,
-          targetProgress,
-          totalAmount
-        );
+      <div className="GoalsList__cards">
+        {props.goals.map((goal: GoalResponse) => {
+          const {
+            title,
+            startDate,
+            endDate,
+            amount,
+            checklist,
+            progress,
+          } = goal.content;
+          const isStarted = shouldShowProgress(
+            startDate,
+            endDate,
+            amount,
+            checklist
+          );
+          const totalAmount = amount || (checklist && checklist.length) || 0;
+          const targetProgress = getTargetProgress(
+            startDate,
+            endDate,
+            totalAmount
+          );
+          const { targetPercent, successPercent } = getProgressPercent(
+            progress,
+            targetProgress,
+            totalAmount
+          );
 
-        return (
-          <Link key={goal.goalId} to={`/goals/${goal.goalId}`}>
-            <GoalCard
-              title={title}
-              endDate={endDate}
-              progress={progress}
-              targetProgress={targetProgress}
-              isStarted={isStarted}
-              targetPercent={targetPercent}
-              successPercent={successPercent}
-              progressTooltip={getProgressTooltip(
-                progress,
-                targetProgress,
-                totalAmount
-              )}
-            />
-          </Link>
-        );
-      })}
+          return (
+            <Link key={goal.goalId} to={`/goals/${goal.goalId}`}>
+              <GoalCard
+                title={title}
+                endDate={endDate}
+                progress={progress}
+                targetProgress={targetProgress}
+                isStarted={isStarted}
+                targetPercent={targetPercent}
+                successPercent={successPercent}
+                progressTooltip={getProgressTooltip(
+                  progress,
+                  targetProgress,
+                  totalAmount
+                )}
+              />
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 };
