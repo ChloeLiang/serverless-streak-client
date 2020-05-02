475fbd06d82b843458eed1805eccbbf3fd2e2ba7
ChloeLiang <nliangxin@gmail.com>
2020-05-02 16:21:10 +0800

    code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import React, { useState, FunctionComponent, useEffect } from 'react';
+import React, {
+  lazy,
+  Suspense,
+  useState,
+  FunctionComponent,
+  useEffect,
+} from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { Layout, Spin } from 'antd';
 import { Auth } from 'aws-amplify';
 import Home from './components/Home';
-import Login from './components/Login';
 import Header from './components/Header';
 import AuthContext from './contexts/AuthContext';
-import SignUp from './components/SignUp';
-import NewGoal from './components/NewGoal';
-import GoalDetails from './components/GoalDetails';
 import ErrorBoundary from './components/ErrorBoundary';
 import { onError } from './services/logger';
+
+const Login = lazy(() => import('./components/Login'));
+const SignUp = lazy(() => import('./components/SignUp'));
+const NewGoal = lazy(() => import('./components/NewGoal'));
+const GoalDetails = lazy(() => import('./components/GoalDetails'));
 
 const App: FunctionComponent = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -34,21 +41,25 @@ const App: FunctionComponent = () => {
 
   const getRoutes = () => {
     let routes = (
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/signup" component={SignUp} />
-        <Route path="/login" component={Login} />
-        <Redirect to="/" />
-      </Switch>
+      <Suspense fallback={<Spin spinning size="large" />}>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/login" component={Login} />
+          <Redirect to="/" />
+        </Switch>
+      </Suspense>
     );
     if (isAuthenticated) {
       routes = (
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/goals/new" component={NewGoal} />
-          <Route path="/goals/:id" component={GoalDetails} />
-          <Redirect to="/" />
-        </Switch>
+        <Suspense fallback={<Spin spinning size="large" />}>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/goals/new" component={NewGoal} />
+            <Route path="/goals/:id" component={GoalDetails} />
+            <Redirect to="/" />
+          </Switch>
+        </Suspense>
       );
     }
     return routes;
