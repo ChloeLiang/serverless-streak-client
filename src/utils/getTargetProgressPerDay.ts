02970d99b868f15726caa6bd51ebbc85c94c03cf
ChloeLiang <nliangxin@gmail.com>
2020-08-13 20:01:18 +0800

    fix getTargetProgressPerDay

--- src/utils/getTargetProgressPerDay.ts
+++ src/utils/getTargetProgressPerDay.ts
@@ -8,7 +8,9 @@ const getTargetProgressPerDay = (
   if (!startDate || !endDate || !amount) {
     return -1;
   }
-  const startOfDay = moment(startDate).startOf('day');
+  const startOfDay = moment().isBefore(moment(endDate))
+    ? moment().startOf('day')
+    : moment(endDate).startOf('day');
   const endOfDay = moment(endDate).endOf('day');
   const period = endOfDay.diff(startOfDay, 'day') + 1;
   return Math.floor(amount / period);
